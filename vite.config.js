4dbc2ee23822bd5e7d923ce7a998a81ae04be6e0
MatthieuLthd <matt.leauthaud@gmail.com>
2026-02-22 14:56:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://matthieulthd.github.io/portfolio-leauthaud",
+  base: "/portfolio-leauthaud",
 })
